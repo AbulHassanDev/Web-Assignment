30c10f61d1fb1d15cc598316841f5c5a548672d6
AbulHassanDev <malikabulhassanm100@gmail.com>
2024-08-27 09:56:12 +0500

    navbar

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
 
-
-const Home  = () => {
+const Home = () => {
   return (
     <>
- 
+      <Navbar />
       <h1 className="text-4xl p-10 font-bold">Welcome to MyPage</h1>
     </>
   );
